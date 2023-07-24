55315385ca3d605fdd079f499356ba1870b142c0
nhsakil <nhsakil1@gmail.com>
2023-07-25 01:07:14 +0600

    Data Structure Completed

---
+++ Access Web Data/Regular Expression.py
@@ -0,0 +1,16 @@
+import re
+handle = open("mbox-short.txt")
+for line in handle:
+    line = line.strip()
+    # if re.search("^From: ",line):
+    #     #print(line)
+    # if re.search("^X-.*:",line):
+    #     print(line)
+    # if re.search("^X-.\S+",line):
+    # #\S = match any non-whitespace character
+    # #+ = one or more times
+    #     print(line)
+    # numbers = re.findall("[0-9]+",line)
+    # print(numbers)
+email = re.findall("^From (\S+@\S+"))
+    print(email)
